c1328eb24a5a28debc5191e04d14f6bea5ca1510
thaylin79 <thaylin.burns@nytimes.com>
2014-12-15 15:13:38 -0500

    change to shell script
    Adding client name to title for client review index

--- scooper.sh
+++ scooper.sh
@@ -3,18 +3,24 @@
 CLIENT_NAME="[CLIENT]"
 PROJECT_NAME="[PROJECT]"
 
+
 ASSET_URL_PATH_RP="---pppath---"
 ASSET_URL="http\:\/\/graphics8\.nytimes\.com\/ads\/paidpost\/$CLIENT_NAME\/$PROJECT_NAME\/"
 
 VIDEO_ASSET_URL_RP="---ppvideopath---"
 VIDEO_ASSET_URL="http\:\/\/video1\.nytimes\.com\/video\/ads\/paidpost\/$CLIENT_NAME\/$PROJECT_NAME\/"
 
+#HOLDING OFF ON THIS UNTIL CAN GET SOMETHING STABLE, MAYBE DO THIS VIA NODE INSTEAD?
+#echo "lets test for curl"
+
+#TEMP_FILE=$(curl --cookie nada --location http://paidpost.nytimes.com/ernst-and-young/its-the-year-of-the-big-deal.html )
+
+
 echo "Building local version into index.html"
 
-cat htmlComponents/nyt5Head.html > index.html
+cat htmlComponents/nyt5Head.html | sed "s/\[CLIENT\]/$CLIENT_NAME/g" > index.html
 cat htmlComponents/body.html | sed "s/$ASSET_URL_PATH_RP//g" | sed "s/$VIDEO_ASSET_URL_RP/vids\//g" >> index.html
 cat htmlComponents/nyt5Foot.html >> index.html
-
 
 echo "Building scoop version into scoop.html"
 
@@ -28,5 +34,8 @@ echo "Building staging version into $1"
 cat htmlComponents/nyt5Head.html > $1
 cat htmlComponents/body.html | sed "s/$ASSET_URL_PATH_RP//g" | sed "s/$VIDEO_ASSET_URL_RP/vids\//g" >> $1
 cat htmlComponents/nyt5Foot.html >> $1
+
+
+
 
 
